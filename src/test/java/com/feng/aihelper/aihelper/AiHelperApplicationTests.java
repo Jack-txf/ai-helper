08c9ac2acf56e0b4017d72809b91c68378af9541
jack_txf <762600503@qq.com>
2026-02-23 16:09:57 +0800

    仓库初始化

---
+++ src/test/java/com/feng/aihelper/aihelper/AiHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feng.aihelper.aihelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
